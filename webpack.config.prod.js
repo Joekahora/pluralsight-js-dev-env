cf31f31176e54a22a2e069ba004cba2720a65508
Joekahora <gachau.joel@gmail.com>
2020-07-07 22:48:31 +0300

    Bundle/Chunk split to save load on time

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -2,20 +2,28 @@ import path from "path";
 import webpack from "webpack";
 import HtmlWebpackPlugin from "html-webpack-plugin";
 
+
 export default{
   debug:true,
   devtools: "source-map",
   noInfo:false,
-  entry:[
-    path.resolve(__dirname,"src/index")
-  ],
+  entry:{
+    vendor: path.resolve(__dirname,"src/vendor"),
+    main: path.resolve(__dirname,"src/index")
+  },
   target:"web",
   output:{
     path: path.resolve(__dirname,"dist"),
     publicPath:"/",
-    filename:"bundle.js"
+    filename:"[name].js"
   },
   plugins:[
+    // Use commonschunkplugin to create a separete bundle
+    // for thrm to be cached easily
+    new webpack.optimize.CommonsChunkPlugin({
+      name: "vendor"
+    }),
+
     // creates html file that includes reference to bundled js
     new HtmlWebpackPlugin({
       template: 'src/index.html',
@@ -32,8 +40,10 @@ export default{
       },
       inject: true
     }),
+
     // eliminate duplicate plugins
     new webpack.optimize.DedupePlugin(),
+
     // uglify the code
     new webpack.optimize.UglifyJsPlugin()
   ],
